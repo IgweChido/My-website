a5d9287d70837d12ab8a16c082b6b063244f6298
Igwe Chidera <52101701+IgweChido@users.noreply.github.com>
2022-01-25 14:02:22 +0100

    website code and styles

---
+++ skills.js
@@ -0,0 +1,32 @@
+/************ SKILLS JS*************/
+
+// Project box hover to show project details
+var desc = document.getElementsByClassName('pdesc')[0];
+
+
+
+	function descShow() {
+
+		
+
+			
+				desc.style.display = "block";
+			
+		}
+
+	
+		
+
+
+
+function descHide() {
+
+		
+			desc.style.display = "none";
+		
+
+		
+
+
+}
+
